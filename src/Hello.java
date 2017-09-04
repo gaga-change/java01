d53652aeebc9e9f0ca7e5f821470368c70951e24
严俊东 <1172482914@qq.com>
2017-09-04 10:58:30 +0800

    添加注释

--- src/Hello.java
+++ src/Hello.java
@@ -1,4 +1,10 @@
+/**
+ * public : 表示这个类是公共类，一个java文件中只能有一个public类
+ * class : 表示这个一个类
+ * Hello : 类名（公共类的类名必须和文件名一致）
+ */
 public class Hello {
+    // 运行文件的入口
     public static void main(String args[]) {
         System.out.println("Hello Java !");
         System.out.println('g');
